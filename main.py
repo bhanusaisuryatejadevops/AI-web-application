44ad14dc5b5d37c2a12a64c3047424595db18af5
surya <surya@gmail.com>
2025-08-25 13:23:35 +0530

    updates

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask, request, jsonify
 from textblob import TextBlob
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+
+# Attach Prometheus monitoring
+metrics = PrometheusMetrics(app)
+metrics.info("app_info", "AI Web Application", version="1.0.0")
 
 @app.route("/")
 def home():
